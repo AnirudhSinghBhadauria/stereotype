1e9de4ee49afa04f2f44fda3f68f890fa8f6f759
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-10-01 16:41:33 +0530

    changed some graphs.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,28 @@ export const metadata = {
     icon: [{ url: "/favicon.ico" }],
     apple: [{ url: "/apple-icon.png" }],
   },
+  openGraph: {
+    url: `${process.env.NEXT_PUBLIC_DOMAIN_URL}`,
+    title: "Stereotype",
+    description:
+      "Discover a world of diverse content in tech, reviews, entertainment, and news. Stereotype - Where Insights, Authenticity, and Inclusivity Meet. Explore now!",
+    images: [
+      {
+        url: "https://cdn.sanity.io/images/aftdl3p2/production/6be92cc3f60e23a4f023a03b92adeb53cd2243c1-1200x630.jpg",
+      },
+    ],
+  },
+  twitter: {
+    title: "Don't live upto your Stereotypes.",
+    description:
+      "Discover a world of diverse content in tech, reviews, entertainment, and news. Stereotype - Where Insights, Authenticity, and Inclusivity Meet. Explore now!",
+    card: "summary_large_image",
+    images: [
+      "https://cdn.sanity.io/images/aftdl3p2/production/6be92cc3f60e23a4f023a03b92adeb53cd2243c1-1200x630.jpg",
+    ],
+    creator: "@LieCheatSteal_",
+    site: `${process.env.NEXT_PUBLIC_DOMAIN_URL}`,
+  },
   other: {
     "google-site-verification": `${process.env.NEXT_PUBLIC_GOOGLE_VER}`,
   },
