ec240e96736a04300c08bdf77678fee9ec9c9580
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-11-22 01:35:06 +0530

    final commit

--- src/components/feedback/feedback-submit-button.tsx
+++ src/components/feedback/feedback-submit-button.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { experimental_useFormStatus as useFormStatus } from "react-dom";
+import { useFormStatus } from "react-dom";
 
 const FeedbackSubmitButton = ({
   formStatus,
