9eee2d3485936f525f73e471146f5d83fe20fc94
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-11-21 16:53:25 +0530

    this is final

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     remotePatterns: [{ hostname: "ik.imagekit.io" }],
   },
